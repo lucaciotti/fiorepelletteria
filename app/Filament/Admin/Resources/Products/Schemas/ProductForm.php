22a2745b6945c236e8a723e3408495b6559d2753
Luca Ciotti <luca.ciotti@gmail.com>
2025-10-31 11:13:41 +0100

    New Admin Panel

---
+++ app/Filament/Admin/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Products\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('code')
+                    ->required(),
+                TextInput::make('description')
+                    ->required(),
+            ]);
+    }
+}
